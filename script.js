a082c2eddd8628ebd2fe9a53cdf90acdca62d879
Benjamin Benoit <119290452+096benjaminbenoit@users.noreply.github.com>
2023-01-14 10:22:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+fetch("https://api.nasa.gov/planetary/apod?api_key=zX3Mvfcz7iJYZeGozyZyHqh4Z8HctwcK2ZURplqK")
+.then(function(response) {
+    return response.json()
+})
+
+.then (function(data) {
+    const infos = data
+    console.log(data)
+
+
+    const mainInfos = document.getElementById('main-infos');
+
+    const newDivInfos = document.createElement('div');
+    mainInfos.append(newDivInfos);
+    newDivInfos.classList.add('infos');
+
+    const newTitle = document.createElement('h2');
+    mainInfos.append(newTitle);
+    newTitle.innerHTML = data.title;
+
+    const link = document.createElement('a');
+    mainInfos.append(link);
+    link.href = data.url;
+    link.getAttribute('target', 'blank');
+
+    const img = document.createElement('img');
+    link.append(img);
+    img.src = data.url;
+
+    const newDivSubInfos = document.createElement('div');
+    newDivSubInfos.classList.add('sub-infos');
+    newDivInfos.append(newDivSubInfos);
+
+    const copyright = document.createElement('h4');
+    newDivSubInfos.append(copyright);
+    copyright.innerHTML = data.copyright;
+
+    const date = document.createElement('h4');
+    newDivSubInfos.append(date);
+    date.innerHTML = data.date;
+
+    const description = document.createElement('p');
+    mainInfos.append(description);
+    description.innerHTML = data.explanation;
+
+})
